e2346ebc77ff1db71df45e4020b6a917213f85b7
Rafael Caetano <rafael.caeta663@gmail.com>
2025-03-05 17:07:04 -0300

    AlunoView lista de disciplinas

--- LAB01/Código/src/EducationalSystemApplication/src/views/AlunoView.java
+++ LAB01/Código/src/EducationalSystemApplication/src/views/AlunoView.java
@@ -1,11 +1,14 @@
 package views;
 
+import models.Disciplina;
+import models.Aluno;
+
 import java.util.Scanner;
 
 import controller.SessaoController;
 
 public class AlunoView {
-    public static void mostrarMenu() {
+    public static void mostrarMenu(Aluno aluno, List<Disciplina> disciplinasDisponiveis) {
         Scanner scanner = new Scanner(System.in);
         int opcao;
 
@@ -17,10 +20,11 @@ public class AlunoView {
             System.out.println("0 - Logout");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Listando disciplinas...");
+                    listarDisciplinas(disciplinasDisponiveis);
                     break;
                 case 2:
                     System.out.println("Realizando matrícula...");
@@ -37,4 +41,20 @@ public class AlunoView {
             }
         } while (opcao != 0);
     }
+    public static void listarDisciplinas(List<Disciplina> disciplinasDisponiveis) {
+        if (disciplinasDisponiveis.isEmpty()) {
+            System.out.println("Não há disciplinas disponíveis para inscrição.");
+            return;
+        }
+
+        System.out.println("\nDisciplinas disponíveis para inscrição:");
+        for (Disciplina disciplina : disciplinasDisponiveis) {
+            System.out.println("- Código: " + disciplina.getCodigo());
+            System.out.println("  Nome: " + disciplina.getNome());
+            System.out.println("  Carga Horária: " + disciplina.getCargaHoraria() + " horas");
+            System.out.println("  Professor: " + disciplina.getProfessor().getNome());
+            System.out.println("  Valor: R$ " + String.format("%.2f", disciplina.getValor()));
+            System.out.println();
+    }
+}
 }
