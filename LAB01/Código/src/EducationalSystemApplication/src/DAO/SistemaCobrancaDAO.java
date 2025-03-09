3f4c15854381388e1f94573903a36294d2754657
Gustavo Delfino Guimarães <delfinoguimaraes1@gmail.com>
2025-03-09 12:23:36 -0300

    update DAO sistema de cobrança

--- LAB01/Código/src/EducationalSystemApplication/src/DAO/SistemaCobrancaDAO.java
+++ LAB01/Código/src/EducationalSystemApplication/src/DAO/SistemaCobrancaDAO.java
@@ -1,22 +1,52 @@
 package DAO;
 
-import models.SistemaSistemaCobranca;
+import models.SistemaCobranca;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class SistemaCobrancaDAO {
-    private Map<Integer, SistemaCobranca> SistemaCobrancas = new HashMap<>();
 
-    public void salvarSistemaCobranca(int idAluno, SistemaCobranca SistemaCobranca) {
-        SistemaCobrancas.put(idAluno, SistemaCobranca);
+    private static final String FILE_NAME = "sistemaCobranca.dat";
+    private static SistemaCobrancaDAO instance = new SistemaCobrancaDAO();
+    private Map<Integer, SistemaCobranca> sistemaCobrancas;
+
+    private SistemaCobrancaDAO() {
+        sistemaCobrancas = new HashMap<>();
+    }
+
+    public static SistemaCobrancaDAO getInstance() {
+        return instance;
+    }
+
+    public void salvarSistemaCobranca(int idAluno, SistemaCobranca sistemaCobranca) {
+        sistemaCobrancas.put(idAluno, sistemaCobranca);
     }
 
     public SistemaCobranca buscarSistemaCobranca(int idAluno) {
-        return SistemaCobrancas.get(idAluno);
+        return sistemaCobrancas.get(idAluno);
     }
 
-    public void atualizarSistemaCobranca(int idAluno, SistemaCobranca SistemaCobranca) {
-        SistemaCobrancas.put(idAluno, SistemaCobranca);
+    public void atualizarSistemaCobranca(int idAluno, SistemaCobranca sistemaCobranca) {
+        sistemaCobrancas.put(idAluno, sistemaCobranca);
     }
+
+    public void excluirSistemaCobranca(int idAluno) {
+        sistemaCobrancas.remove(idAluno);
+    }
+    
+
+    // private Map<Integer, SistemaCobranca> SistemaCobrancas = new HashMap<>();
+
+    // public void salvarSistemaCobranca(int idAluno, SistemaCobranca SistemaCobranca) {
+    //     SistemaCobrancas.put(idAluno, SistemaCobranca);
+    // }
+
+    // public SistemaCobranca buscarSistemaCobranca(int idAluno) {
+    //     return SistemaCobrancas.get(idAluno);
+    // }
+
+    // public void atualizarSistemaCobranca(int idAluno, SistemaCobranca SistemaCobranca) {
+    //     SistemaCobrancas.put(idAluno, SistemaCobranca);
+    // }
 }
