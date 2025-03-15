f35687171ae650f806ffb87915e0cf9118042f82
JuliaMedeir0s <julia.medeiros1159@gmail.com>
2025-03-15 20:41:51 -0300

    Cria aplicação

---
+++ LAB02/sistema-aluguel-carros/src/test/java/com/lab02/sistema_aluguel_carros/SistemaAluguelCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab02.sistema_aluguel_carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAluguelCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
