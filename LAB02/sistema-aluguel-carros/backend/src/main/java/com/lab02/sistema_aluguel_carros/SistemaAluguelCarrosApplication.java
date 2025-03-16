7b51d94a0fb63d6b3adb434a900fb2568612faee
JuliaMedeir0s <julia.medeiros1159@gmail.com>
2025-03-15 22:09:42 -0300

    Add aplicação

---
+++ LAB02/sistema-aluguel-carros/backend/src/main/java/com/lab02/sistema_aluguel_carros/SistemaAluguelCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.lab02.sistema_aluguel_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAluguelCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAluguelCarrosApplication.class, args);
+	}
+
+}
