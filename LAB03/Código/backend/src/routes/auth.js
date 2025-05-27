7aecd3acbda1db822d34405a8796fc2e44ea1c83
Gustavo Delfino Guimarães <delfinoguimaraes1@gmail.com>
2025-05-26 21:50:11 -0300

    delete backend

--- LAB03/Código/backend/src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.route('/')
-    .post(authController.login);
-
-module.exports = router;
